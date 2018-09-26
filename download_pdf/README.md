b7ad3e1b89a5c78a6cfdc8845c18c600ada4b681
Go Watanabe <gowatana@vmtn.jp>
2018-09-26 10:38:34 +0900

    add download_pdf.sh

---
+++ download_pdf/README.md
@@ -0,0 +1,6 @@
+PDFファイルをダウンロード。HCIでしぼるため "grep HCI"
+
+```
+$ cd download_pdf/
+$ sh download_pdf.sh
+```
---
+++ download_pdf/download_pdf.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PDF_LIST=`cat ../vmworld-us-playback-urls.md | grep '\[PDF\]' | grep HCI | sed "s|.*\[PDF\]||g" | sed s/[\(\)]//g`
+TOTAL=`echo "$PDF_LIST" | wc -l | xargs`
+
+DL_COUNT=0
+
+echo "$PDF_LIST" | while read L
+do
+    DL_COUNT=`expr $DL_COUNT + 1`
+    echo "File $DL_COUNT/$TOTAL: $L"
+    curl -s -O $L
+done
+
